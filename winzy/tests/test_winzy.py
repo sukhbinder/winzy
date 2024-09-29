207c928f7d0c8e4f1a4f98dcca7f43a98506c081
Sukhbinder Singh <sukh2010@yahoo.com>
2024-09-29 15:50:43 +0530

    Create test_winzy.py

---
+++ winzy/tests/test_winzy.py
@@ -0,0 +1,23 @@
+import sys
+from unittest import mock
+import pytest
+from winzy.cli import main
+
+def test_show_help(capsys):
+    with mock.patch('sys.argv', ['winzy']):
+        with mock.patch('winzy.plugins.load_plugins', []):
+            main()
+            captured = capsys.readouterr()
+            assert "usage: winzy [-h]" in captured.out
+
+def test_plugins(capsys):
+    with mock.patch('sys.argv', ['winzy', 'plugins']):
+        main()
+        captured = capsys.readouterr()
+        assert "Installed Plugins:" in captured.out
+
+def test_invalid_command():
+    with mock.patch('sys.argv', ['winzy', 'invalid_command']):
+        with pytest.raises(SystemExit) as e:
+            main()
+        assert str(e.value) == '2'  # SystemExit should be raised with code 2 (error)
