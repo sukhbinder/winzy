9cc3238d9b23fa0b03905c44b20375d031105fe9
Sukhbinder Singh <sukh2010@yahoo.com>
2024-09-29 15:40:00 +0530

    Update cli.py

--- winzy/cli.py
+++ winzy/cli.py
@@ -1,5 +1,5 @@
 
-from .plugins import pm, get_plugins, load_plugins
+from .plugins import pm, get_plugins, load_plugins, install_plugin
 import argparse
 
 
@@ -7,8 +7,19 @@ def show_help(args):
     print("winzy update")
 
 
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _format_action(self, action):
+        if isinstance(action, argparse._SubParsersAction):
+            return "\n".join(f" {name:<20} {sub.description}" for name, sub in action.choices.items()) + "\n"
+        return super()._format_action(action)
+
+
+def install_cmd(args):
+    install_plugin(args.packages, args.upgrade, args.editable, args.force_reinstall, args.no_cache_dir)
+
 def main():
-    parser = argparse.ArgumentParser(description="Win Utils for common tasks in windows")
+    parser = argparse.ArgumentParser(description="Win Utils for common tasks in windows",
+                                     formatter_class=CustomHelpFormatter)
     parser.set_defaults(func=show_help)
     
     subparser = parser.add_subparsers(dest="command")
@@ -16,6 +27,15 @@ def main():
     # add plugins command
     pugs_p = subparser.add_parser("plugins", description="Get all listed plugins")
     pugs_p.set_defaults(func=get_plugins)
+
+    install_parser = subparser.add_parser('install', description="Install plugins in the same environemnt as winzy")
+    install_parser.add_argument('--upgrade', action='store_true')
+    install_parser.add_argument('--editable', help='Edit mode for packages')
+    install_parser.add_argument('--force-reinstall', action='store_true')
+    install_parser.add_argument('--no-cache-dir', action='store_true')
+    install_parser.add_argument('packages', nargs='+')
+    install_parser.set_defaults(func=install_cmd)
+
 
     load_plugins()
     
