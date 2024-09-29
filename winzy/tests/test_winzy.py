3d444ed17ae482f0ae9c29c985c0a374b459a182
Sukhbinder Singh <sukh2010@yahoo.com>
2024-09-29 16:07:53 +0530

    Update test_winzy.py

--- winzy/tests/test_winzy.py
+++ winzy/tests/test_winzy.py
@@ -14,7 +14,7 @@ def test_plugins(capsys):
     with mock.patch('sys.argv', ['winzy', 'plugins']):
         main()
         captured = capsys.readouterr()
-        assert "Installed Plugins:" in captured.out
+        assert "No external plugins in env." in captured.out
 
 def test_invalid_command():
     with mock.patch('sys.argv', ['winzy', 'invalid_command']):
